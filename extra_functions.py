45d7298eb046d9df5d13ba6c69e52a24f17a7382
Pedro-PC <vaz.valois@df.ufscar.br>
2018-05-01 00:35:02 -0300

    new module for functional programming

---
+++ extra_functions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 12:20:27 2018
+
+@author: Pedro
+
+Useful extra functions
+"""
+
+import numpy as np
+
+def remove_non_ascii(string):
+    new_string = []
+    for i in string:
+        if ord(i) < 128:
+             new_string.append(i)
+        else: pass
+    
+    return ''.join(new_string)
+
+def is_number(string):
+    try:               float(string)
+    except ValueError: return False
+    return True
+
+def magnitude(number):
+    return int( np.floor( np.log10( number )) )
