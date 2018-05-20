0251ea0660effd8a4a88ca4c2eb4fbaf7dde48b0
Pedro-PC <vaz.valois@df.ufscar.br>
2018-05-20 14:17:29 -0300

    adapted for linux and windows

--- appdemo.py
+++ appdemo.py
@@ -9,16 +9,28 @@ Created on Wed May  2 23:42:56 2018
 from pellet import Pellet
 from nist import NIST
 import time
+import sys
+
 start = time.time()
-    
-path1 = r'/home/pedro/PythonProjects/LIBS/data/1'
-tb1 = Pellet(path1)
-    
-path2 = r'/home/pedro/PythonProjects/LIBS/data/18'
-tb18 = Pellet(path2)
-    
-path3 = r'/home/pedro/PythonProjects/LIBS/data/23'
-tb23 = Pellet(path3)
+
+ubuntu = [r'/home/pedro/PythonProjects/LIBS/data/1',
+          r'/home/pedro/PythonProjects/LIBS/data/18',
+          r'/home/pedro/PythonProjects/LIBS/data/23']
+windows = [r'C:\Users\Pedro\Google Drive\Iniciação Científica - EMBRAPA - 2017\Programas\LIBS\data\1', 
+           r'C:\Users\Pedro\Google Drive\Iniciação Científica - EMBRAPA - 2017\Programas\LIBS\data\18',
+           r'C:\Users\Pedro\Google Drive\Iniciação Científica - EMBRAPA - 2017\Programas\LIBS\data\23']    
+
+paths = []
+if 'linux' in sys.platform:
+    for path in ubuntu:
+        paths.append(path)
+else:
+    for path in windows:
+        paths.append(path)
+
+tb1 = Pellet(paths[0]) 
+tb18 = Pellet(paths[1])
+tb23 = Pellet(paths[2])
             
 tb1.drop_outliers(reference = tb1.avg_spectra)
 tb18.drop_outliers(reference = tb18.avg_spectra)
@@ -41,7 +53,7 @@ db = NIST(elements = ['C I', 'B I', 'K I', 'P I', 'N I', 'H I', 'Cu I',
     #tem um erro acontecendo, confira fazendo n_picos/n_pontos do espectro.
 
 psbty1 = tb1.peak_possibilites(db.table, N = 1, avg = 1, ret_unknown = 1)
-#psbty1_count = psbty1.value_counts()
+psbty1_count = psbty1['possibilities']
     
 end = time.time()
 total_time = end - start#N = 5: 90s, N = 1: 43s PC - N = 1: 80s.
